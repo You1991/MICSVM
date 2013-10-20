4e80c3dbc5671c81d291635ec934b5015642eb8c
You, Yang (Forrest) <forrest1991@foxmail.com>
2013-10-21 04:16:05 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ export PHI_USE_2MB_BUFFERS=16K
 echo $PHI_KMP_AFFINITY
 #export OMP_NUM_THREADS=16
 icpc -o mic_svm -openmp -simd -vec-report1 sparse_caching.cpp
-./mic_svm -c 1.0 -g 0.031250 -o forest12000.mdl ./dataset/real_sim
+./mic_svm -c 0.1  -g 0.0625 -o adult.mdl ./adult
 
